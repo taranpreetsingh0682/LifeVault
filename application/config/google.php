8da231c03475fc0b1e6c0f83900496e21e8bef24
taran-ai <taranpreetsingh62006@gmail.com>
2026-08-18 18:08:44 +0530

    Fix Google OAuth redirect environment variables

--- application/config/google.php
+++ application/config/google.php
@@ -6,4 +6,4 @@ $config['google_client_id'] = getenv('GOOGLE_CLIENT_ID') ?: '';
 $config['google_client_secret'] = getenv('GOOGLE_CLIENT_SECRET') ?: '';
 
 
-$config['google_redirect_uri'] = getenv('GOOGLE_REDIRECT_URL') ?: '';
+$config['google_redirect_uri'] = getenv('GOOGLE_REDIRECT_URI') ?: '';
